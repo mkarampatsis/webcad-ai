0bbd6bc40b3029f9728261d92da583be71a6093f
mkarampatsis <marka@central.ntua.gr>
2026-07-08 14:16:03 +0300

    changes to floor elements

--- main.py
+++ main.py
@@ -21,6 +21,62 @@ def agent():
           "type": "add_floor",
           "parameters": {"width": 8, "length": 10, "elevation": 3},
         }
+        # {
+        #   "type": "add_floor",  
+        #   "settings": {
+        #     "units": {
+        #       "length": "m",
+        #       "force": "kN",
+        #       "moment": "kNm",
+        #       "stress": "MPa",
+        #       "density": "kg/m3"
+        #     }
+        #   },
+        #   "nodes": {
+        #     "1": {"x": 0, "y": 3, "z": 0},
+        #     "2": {"x": 5, "y": 3, "z": 0},
+        #     "3": {"x": 5, "y": 3, "z": 5},
+        #     "4": {"x": 0, "y": 3, "z": 5}
+        #   },
+        #   "members": {
+        #     "1": {"node_A": 1, "node_B": 2, "section_id": 1, "fixity_A": "FFFFFF", "fixity_B": "FFFFFF"},
+        #     "2": {"node_A": 2, "node_B": 3, "section_id": 1, "fixity_A": "FFFFFF", "fixity_B": "FFFFFF"},
+        #     "3": {"node_A": 3, "node_B": 4, "section_id": 1, "fixity_A": "FFFFFF", "fixity_B": "FFFFFF"},
+        #     "4": {"node_A": 4, "node_B": 1, "section_id": 1, "fixity_A": "FFFFFF", "fixity_B": "FFFFFF"}
+        #   },
+        #   "plates": {
+        #     "1": {
+        #       "node_A": 1,
+        #       "node_B": 2,
+        #       "node_C": 3,
+        #       "node_D": 4,
+        #       "material_id": 1,
+        #       "thickness": 200,
+        #       "rotZ": 0,
+        #       "type": "plate"
+        #     }
+        #   },
+        #   "materials": {
+        #     "1": {
+        #       "name": "Concrete",
+        #       "E": 30000,
+        #       "poisson": 0.2,
+        #       "density": 2500,
+        #       "class": "concrete"
+        #     }
+        #   },
+        #   "sections": {
+        #     "1": {
+        #       "name": "Concrete Beam",
+        #       "area": 0.12,
+        #       "iz": 0.0016,
+        #       "iy": 0.0009,
+        #       "j": 0.002,
+        #       "material_id": 1
+        #     }
+        #   }
+        # }
+
       ]
     })
 
